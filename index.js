e1ca2fc060f384a772644d96dc71086c71f700c4
crossjs <liwenfu@crossjs.com>
2016-09-14 20:54:30 +0800

    bugfix

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = require('./lib');
+'use strict';
+
+var postcss = require('postcss')
+var flexible = require('./lib/flexible')
+
+module.exports = postcss.plugin('postcss-flexible', function (options) {
+  return function (css, result) {
+    result.root = postcss.parse(flexible(css.toString(), options))
+  }
+})
